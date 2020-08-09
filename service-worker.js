e6a63c8a9676b25cab305729425f7b6bb3981dfa
Ando Ritonga <58759006+kuro21@users.noreply.github.com>
2020-08-09 10:14:56 +0700

    V2.1

--- service-worker.js
+++ service-worker.js
@@ -11,8 +11,15 @@ else
     { url: '/team.html', revision: '1' },
     { url: '/nav.html', revision: '1' },
     { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/manifest.json', revision: '1' },
+    { url: '/js/api.js', revision: '1' },
+    { url: '/js/db.js', revision: '1' },
+    { url: '/js/idb.js', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/nav.js', revision: '1' },
+    { url: '/js/script.js', revision: '1' },
 ], {
-ignoreURLParametersMatching: [/.*/]
+ignoreUrlParametersMatching: [/.*/]
 });
 
 workbox.routing.registerRoute(
@@ -24,14 +31,6 @@ workbox.routing.registerRoute(
   new RegExp('/pages/'),
     workbox.strategies.staleWhileRevalidate({
         cacheName: 'pages'
-    })
-);
-
-
-workbox.routing.registerRoute(
-  new RegExp('/js/'),
-    workbox.strategies.staleWhileRevalidate({
-        cacheName: 'js'
     })
 );
 
